c5ba35985f90afda7ea9192ddd3444a771cceed1
chenxiaohui <chen.xiaohui@yrw.com>
2017-12-25 14:12:40 +0800

    添加ehcache

--- src/main/java/com/gatecm/tip/controller/TipController.java
+++ src/main/java/com/gatecm/tip/controller/TipController.java
@@ -1,6 +1,8 @@
 package com.gatecm.tip.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache.ValueWrapper;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.databind.deser.Deserializers.Base;
 import com.gatecm.tip.constant.BaseConstant;
+import com.gatecm.tip.entity.TipContent;
 import com.gatecm.tip.service.Rrs;
 import com.gatecm.tip.service.TipContentService;
 
@@ -23,6 +26,9 @@ import com.gatecm.tip.service.TipContentService;
 public class TipController {
 
 	@Autowired
+	private CacheManager cacheManager;
+
+	@Autowired
 	private TipContentService tipContentService;
 
 	@RequestMapping(value = "/write")
@@ -32,9 +38,18 @@ public class TipController {
 
 	@RequestMapping(value = "/{tipId}/edit")
 	public ModelAndView write(@PathVariable Long tipId) {
+		ValueWrapper valueWrapper = cacheManager.getCache("tip").get(tipId);
+		if(valueWrapper!=null){
+			Rrs rrs = (Rrs)valueWrapper.get();
+		 System.err.println("cache: "+rrs.getResult());
+		 System.err.println("cache tip: "+rrs.getData());
+		 TipContent tip = (TipContent) rrs.getData();
+		 System.err.println("cache tipId: "+tip.getId());
+		}
+		 
 		ModelAndView modelAndView = new ModelAndView();
 		Rrs rrs = tipContentService.getDraftTip(tipId);
-		modelAndView.addObject(BaseConstant.RESPONSE_DATA,rrs);
+		modelAndView.addObject(BaseConstant.RESPONSE_DATA, rrs);
 		modelAndView.setViewName("tip/write");
 		return modelAndView;
 	}
