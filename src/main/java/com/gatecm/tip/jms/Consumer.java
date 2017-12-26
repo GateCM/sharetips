bb2aca4bcfa69ccda94f62193a32fdce32b021ee
chenxiaohui <chen.xiaohui@yrw.com>
2017-12-26 17:19:54 +0800

    集成activeMQ

---
+++ src/main/java/com/gatecm/tip/jms/Consumer.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.gatecm.tip.jms;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+import com.gatecm.tip.entity.MemberBasic;
+
+/**
+ * @Description: TODO()
+ * @author chenxiaohui
+ * @date 2017年12月26日 上午10:08:04
+ *
+ */
+@Component
+public class Consumer {
+
+	@JmsListener(destination = "mytest.queue")
+	@SendTo("out.queue")
+	public String receiveQueue(MemberBasic text) {
+		System.err.println("Consumer收到的报文为：" + text.getPassword());
+		return "return：" + text.getPassword();
+	}
+
+}
---
+++ src/main/java/com/gatecm/tip/jms/Producer.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.gatecm.tip.jms;
+
+import javax.jms.Destination;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import com.gatecm.tip.entity.MemberBasic;
+
+/**
+ * @Description: TODO()
+ * @author chenxiaohui
+ * @date 2017年12月26日 上午10:04:49
+ *
+ */
+@Service("producer")
+public class Producer {
+	@Autowired
+	// 也可以注入JmsTemplate，JmsMessagingTemplate对JmsTemplate进行了封装
+	private JmsMessagingTemplate jmsMessagingTemplate;
+
+	// 发送消息，destination是发送到的队列，message是待发送的消息
+	public void sendMessage(Destination destination, MemberBasic message) {
+		jmsMessagingTemplate.convertAndSend(destination, message);
+	}
+
+	@JmsListener(destination = "out.queue")
+	public void receiveMessage(String text) throws InterruptedException {
+		Thread.sleep(100);
+		System.err.println("返回的报文：" + text);
+	}
+
+}
